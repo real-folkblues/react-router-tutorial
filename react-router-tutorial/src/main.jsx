87d7652f6c7628c0adac5cc87dd4473dc82713e3
real-folkblues <marenicolor@gmail.com>
2024-07-06 13:23:25 -0400

    finished createContact action and route, added editContact action and route, edit contact worked but hitting new button the first time created multiple 'no names', then after selecting a no name and editing successfully, i clicked new again and it did nothing. once i clicked on a different no name, the New button did bring up the new contact form.

--- react-router-tutorial/src/main.jsx
+++ react-router-tutorial/src/main.jsx
@@ -1,16 +1,16 @@
-import Contact from "./routes/contact";
-import Root, { loader as rootLoader } from "./routes/root";
-
+import Contact, {loader as contactLoader, }from "./routes/contact";
+import Root, {
+  loader as rootLoader,
+  action as rootAction,
+} from "./routes/root";
+import EditContact, {action as editAction, } from "./routes/edit";
 
 // import Root from "./routes/root";
 import ErrorPage from "./error-page";
 
 import * as React from "react";
 import * as ReactDOM from "react-dom/client";
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
 
 const router = createBrowserRouter([
@@ -18,14 +18,22 @@ const router = createBrowserRouter([
     path: "/",
     element: <Root />,
     errorElement: <ErrorPage />,
-  loader: rootLoader,
-  children: [
-    {
-    path: "contacts/:contactId",
-    element: <Contact />,
-    },
-  ]
-  }
+    loader: rootLoader,
+    action: rootAction,
+    children: [
+      {
+        path: "contacts/:contactId",
+        element: <Contact />,
+        loader: contactLoader,
+      },
+      {
+        path: "contacts/:contactId/edit",
+        element: <EditContact />,
+        loader: contactLoader, 
+        action: editAction,
+      },
+    ],
+  },
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
--- react-router-tutorial/src/routes/root.jsx
+++ react-router-tutorial/src/routes/root.jsx
@@ -1,19 +1,27 @@
-import { Outlet, Link, useLoaderData } from "react-router-dom";
-import { getContacts } from "../contacts";
+import { Outlet, Link, useLoaderData, Form, redirect } from "react-router-dom";
+import { getContacts, createContact } from "../contacts";
+
+
+export async function action() {
+  const contact = await createContact();
+  return redirect(`/contacts/${contact.id}/edit`);
+}
 
 export async function loader(){
     const contacts = await getContacts();
     return { contacts };
 }
 
+
+
 export default function Root() {
-    const contacts = useLoaderData();
+    const { contacts } = useLoaderData();
   return (
     <>
       <div id="sidebar">
         <h1>React Router Contacts</h1>
         <div>
-          <form id="search-form" role="search">
+          <Form id="search-form" role="search">
             <input
               id="q"
               aria-label="Search contacts"
@@ -30,24 +38,13 @@ export default function Root() {
               className="sr-only"
               aria-live="polite"
             ></div>
-          </form>
-          <form method="post">
+          </Form>
+          <Form method="post">
             <button type="submit">New</button>
-          </form>
+          </Form>
         </div>
         <nav>
-            
-          <ul>
-            <li>
-              <Link to={`/contacts/1`}>Your Name</Link>
-            </li>
-            <li>
-              <Link to={`/contacts/2`}>Your Friend</Link>
-            </li>
-          </ul>
-        </nav>
-        <nav>
-                      {contacts.length ? (
+        {contacts.length ? (
             <ul>
               {contacts.map((contact) => (
                 <li key={contact.id}>
